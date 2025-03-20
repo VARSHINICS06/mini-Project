671c830c828393960c19fae107bd73bda20212a6
Varshini C S <varshinics123@gmail.com>
2025-03-20 21:37:50 +0530

    Add files via upload

---
+++ Guess-My-Number/script.js
@@ -0,0 +1,60 @@
+
+// let secretNumber=document.querySelector('.number');
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let highScore=0;
+
+let displaymessage=function(message){
+    document.querySelector('.message').textContent=message;
+};
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess=Number(document.querySelector('.guess').value);
+    
+    if(!guess){
+        // document.querySelector('.message').textContent='No number'
+        displaymessage('No Number')
+    }
+    else if(guess===secretNumber){
+        // document.querySelector('.message').textContent='correct Number'
+        displaymessage("correct Number")
+        document.querySelector('.number').textContent=secretNumber;
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+        if(score>highScore){
+            highScore=score;
+            document.querySelector('.highscore').textContent=highScore;
+        }
+
+               
+
+
+    }
+    else if(guess!==secretNumber){
+        if(score>1){
+            // document.querySelector('.message').textContent=guess>secretNumber? 'Too high': 'Too Low';
+            displaymessage(guess>secretNumber? 'Too high': 'Too Low');
+            score--;
+            document.querySelector('.score').textContent=score;
+        }
+        else{
+            // document.querySelector('.message').textContent="You lost the game";
+            displaymessage("You lost the game");
+            document.querySelector('.score').textContent=0;
+
+        }
+    }
+});
+document.querySelector('.again').addEventListener('click',function(){
+    score=20;
+    secretNumber=Math.trunc(Math.random()*20)+1;
+    // document.querySelector('.message').textContent="Start guessing";
+    displaymessage("Start guessing")
+    document.querySelector('.score').textContent=score;
+    document.querySelector('.number').textContent='?';
+    document.querySelector('.guess').textContent=' ';
+    document.querySelector('body').style.backgroundColor='#222';
+    document.querySelector('.number').style.width='15rem';
+
+
+});
