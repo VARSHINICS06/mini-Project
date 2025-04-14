1e2473042a8a6c38f996d42352dd33a08c2105fc
Varshini C S <varshinics123@gmail.com>
2025-04-14 20:11:59 +0530

    Update script.js

--- Bankist App/script.js
+++ Bankist App/script.js
@@ -61,6 +61,64 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+
+const displayMovements=function(movements){
+  containerMovements.innerHTML='';
+
+  movements.forEach(function(mov,i){
+    const type=mov>0?'deposit':'withdrawal';
+    const html=`
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1 } ${type}</div>
+  
+          <div class="movements__value">${mov}</div>
+        </div>`;
+        containerMovements.insertAdjacentHTML('afterbegin',html);
+  });
+};
+
+const calcDisplayBalance=function(acc){
+   acc.balance=acc.movements.reduce((acc,mov)=>acc+mov,0);
+  labelBalance.textContent=`${acc.balance} EUR`;
+
+};
+
+const calcDisplaySummary=function(acc){
+  const income=acc.movements.filter(mov=>mov>0)
+  .reduce((acc,mov)=>acc+mov,0);
+  labelSumIn.textContent=income;
+  const out=acc.movements.filter(mov=>mov<0)
+  .reduce((acc,mov)=>acc+mov,0);
+  labelSumOut.textContent=income;
+  
+  const interest=acc.movements.filter(mov=>mov>0)
+  .map(desposit=>(desposit*acc.interestRate)/100)
+  .filter(int=>int>=1)
+  .reduce((acc,int)=>acc+int,0)
+  labelSumInterest.textContent=interest;
+  };
+
+
+const createUserName=function(accs){
+  accs.forEach(function(acc){
+    acc.userName=acc.owner.toLowerCase().split(' ').map(name=>name[0]).join('');
+  });
+};
+createUserName(accounts);
+const updateUI=function(acc){
+  //Display movement
+  displayMovements(acc.movements);
+  //Display balance
+  calcDisplayBalance(acc);
+  //Display summary
+  calcDisplaySummary(acc);
+
+};
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +132,74 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+//Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click',function(e){
+  e.preventDefault();
+  currentAccount=accounts.find(acc=>acc.userName===inputLoginUsername.value);
+  console.log(currentAccount);
+  if(currentAccount?.pin===Number(inputLoginPin.value)){
+    
+    labelWelcome.textContent=`Welcome back,${currentAccount.owner.split(' ')[0]}`;
+    containerApp.style.opacity=100;
+  };
+  //Clear inputfiled
+  inputLoginPin.value=inputLoginUsername.value=' ';
+  inputLoginPin.blur();
+  //Update UI
+  updateUI(currentAccount);
+  
+
+});
+btnTransfer.addEventListener('click',function(e){
+  e.preventDefault();
+  const amount=Number(inputTransferAmount.value);
+  const receiverAcc=accounts.find(acc=>acc.userName===inputTransferTo.value);
+  inputTransferAmount.value=inputTransferTo.value='';
+
+ 
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.userName !== currentAccount.userName
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.userName &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.userName === currentAccount.userName
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
